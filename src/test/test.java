4273978e04b5a23064de42fbf2979c371496f8ae
lhx510 <1297154113@qq.com>
2016-03-07 16:49:52 +0800

    update

--- src/test/test.java
+++ src/test/test.java
@@ -7,7 +7,11 @@ import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
 
 public class test {
-
+    //安装数量
+    //到期时间
+    //用户设备数量
+    //组数量
+    //功能点
     public static void main(String[] args) {
         JFrame frame = new JFrame("证书");
         frame.addWindowListener(new WindowAdapter() {
